9be0021a40e69b076bb73486dfa72e2ba9f52519
Gilad Freidkin <fre.gilad@cscomp.cs.technion.ac.il>
2022-12-02 19:15:13 +0200

    update

--- SyntaxNode.cpp
+++ SyntaxNode.cpp
@@ -7,88 +7,199 @@ using std::string;
 enum class ObjectType { Void, Int, Bool, Byte, String };
 enum class TernaryOperator { TernaryIf };
 enum class BinaryOperator { Add, Sub, Mul, Div, And, Or, Equal, NotEqual, LessThan, GreaterThan, LessThanEqual, GreaterThanEqual };
-enum class UnaryOperator { Not, Cast };
-enum class BranchType {Break, Continue };
+enum class UnaryOperator { Not };
+enum class BranchType { Break, Continue };
 
 class SyntaxNode
 {
     virtual vector<SyntaxNode*> GetParent() = 0;
-    
+
     virtual vector<SyntaxNode*> GetChildren() = 0;
 };
 
 class Expression : public SyntaxNode
 {
-    ObjectType return_type;
+    public:
+
+    ObjectType ReturnType;
+
+    Expression(ObjectType type)
+    {
+        ReturnType = type;
+    }
+};
+
+class CastExpression : public Expression
+{
+    public:
+
+    TypeNode* typeNode;
+    Expression* exp;
+
+    CastExpression(TypeNode* typeNode, Expression* exp) : Expression(typeNode->type)
+    {
+        ObjectType srcType = exp->ReturnType;
+        ObjectType dstType = typeNode->type;
+
+        if (dstType != ObjectType::Int && dstType != ObjectType::Byte)
+        {
+            // todo: handle illigal cast
+        }
+
+        if (srcType != ObjectType::Int && srcType != ObjectType::Byte)
+        {
+            // todo: handle illigal cast
+        }
+
+        this->typeNode = typeNode;
+        this->exp = exp;
+    }
 };
 
 class UnaryExpression : public Expression
 {
+    public:
+
     Expression* exp;
     UnaryOperator operator_type;
+
+    UnaryExpression(Expression* expression, UnaryOperator op) : Expression(expression->ReturnType)
+    {
+        if (operator_type == UnaryOperator::Not)
+        {
+            if (exp->ReturnType != ObjectType::Bool)
+            {
+                // todo: handle error
+            }
+
+            if (ReturnType != ObjectType::Bool)
+            {
+                // todo: handle error
+            }
+
+            exp = expression;
+            operator_type = op;
+        }
+    }
 };
 
 class BinaryExpression : public Expression
 {
+    public:
+
     Expression* left;
     Expression* right;
     BinaryOperator operator_type;
+
+    BinaryExpression(Expression* l, Expression* r, BinaryOperator op)
+    {
+        if (BinaryExpression::IsLogicalOperator(op))
+        {
+            if (l->ReturnType != ObjectType::Bool || r->ReturnType != ObjectType::Bool)
+            {
+                //todo: handle error
+            }
+        }
+
+        if (BinaryExpression::IsNumericOperator(op))
+        {
+
+        }
+    }
+
+    static bool IsLogicalOperator(BinaryOperator op)
+    {
+        return op == BinaryOperator::Or || op == BinaryOperator::And;
+    }
+
+    static bool IsNumericOperator(BinaryOperator op)
+    {
+        return op == BinaryOperator::Add || op == BinaryOperator::Sub || op == BinaryOperator::Mul || op == BinaryOperator::Div;
+    }
+
+    static bool IsNumericType(ObjectType type)
+    {
+        return type == ObjectType::Byte || type == ObjectType::Int;
+    }
+
+    static ObjectType GetReturnType(ObjectType left, ObjectType right)
+    {
+
+    }
 };
 
 class TernaryExpression : public Expression
 {
-    Expression* left;
-    Expression* middle;
-    Expression* right;
-    TernaryOperator operator_type;
+    public:
+
+    Expression* Left;
+    Expression* Middle;
+    Expression* Right;
+    TernaryOperator Operator;
 };
 
 class ConstExpression : public Expression
 {
-    union Litteral 
+    public:
+
+    Litteral Value;
+    ObjType ValueType;
+
+    union Litteral
     {
         int int_val;
         bool bool_val;
         char byte_val;
         char* str_val;
     };
-
-    Litteral value;
 };
- 
+
 class ArgumentList : public SyntaxNode
 {
-    vector<Expression*> arguments;
-}; 
+    public:
 
- class CallExpression : public Expression
+    vector<Expression*> arguments;
+};
+
+class CallExpression : public Expression
 {
+    public:
+
     ArgumentList* argument_list;
     string identifier;
 };
 
 class IdentifierExpression : public Expression
 {
+    public:
+
     string identifier;
 };
 
 class TypeNode : public SyntaxNode
 {
+    public:
+
     ObjectType type;
 };
 
 class Statement : public SyntaxNode
 {
-    
+    public:
+
 };
 
 class StatementsList : SyntaxNode
 {
+    public:
+
     vector<Statement*> statements;
 };
 
 class IfStatement : public Statement
 {
+    public:
+
     Expression* condition;
     Statement* body;
     Expression* else_clause;
@@ -96,28 +207,38 @@ class IfStatement : public Statement
 
 class WhileStatement : public Statement
 {
+    public:
+
     Expression* condition;
     Statement* body;
 };
 
 class BranchStatement : public Statement
 {
+    public:
+
     BranchType type;
 };
 
 class ReturnStatement : public Statement
 {
+    public:
+
     Expression* expression;
     ObjectType return_type;
 };
 
 class CallStatement : public Statement
 {
+    public:
+
     CallExpression* call;
 };
 
 class AssignStatement : public Statement
 {
+    public:
+
     string identifier;
     ObjectType type;
     Expression* value;
@@ -125,41 +246,54 @@ class AssignStatement : public Statement
 
 class DeclarationStatement : public Statement
 {
-    string identifier;
-    ObjectType type;
-    Expression* value;
+    public:
+
+    string Identifier;
+    ObjectType Type;
+    Expression* Value;
 };
 
 class BlockStatement : public Statement
 {
-    StatementsList* statements;
+    public:
+
+    StatementsList* StatementList;
 };
 
 class FuncDeclaration : SyntaxNode
 {
-    TypeNode* return_type;
-    string identifier;
-    ParameterList* parametes;
-    StatementsList* body;
+    public:
+
+    TypeNode* ReturnType;
+    string Identifier;
+    ParameterList* ParameterList;
+    StatementsList* Body;
 };
 
 class ParameterList : SyntaxNode
 {
+    public:
 
 };
 
 class Parameter : SyntaxNode
 {
-    ObjectType type;
-    string identifier;
+    public:
+
+    ObjectType Type;
+    string Identifier;
 };
 
-class FuncList : SyntaxNode
+class FunctionList : SyntaxNode
 {
-    vector<FuncDeclaration*> funcs;
+    public:
+
+    vector<FuncDeclaration*> Functions;
 };
 
 class GlobalScope : SyntaxNode
 {
-    FuncList* funcs;
+    public:
+
+    FunctionList* FunctionList;
 };
