2998d154815d23d4fb27cc8ac4620f9c75689b11
giladfrid009 <freidkin.gilad@gmail.com>
2022-12-17 11:54:32 +0200

    .

--- hw3_output.hpp
+++ hw3_output.hpp
@@ -3,32 +3,31 @@
 
 #include <vector>
 #include <string>
-using namespace std;
 
 namespace output
 {
     void end_scope();
 
-    void print_id(const string& id, int offset, const string& type);
+    void print_id(const std::string& id, int offset, const std::string& type);
 
     /* Do not save the string returned from this function in a data structure
         as it is not dynamically allocated and will be destroyed(!) at the end of the calling scope.
     */
-    string make_function_type(const string& ret_type, vector<string>& arg_types);
+    std::string make_function_type(const std::string& ret_type, std::vector<std::string>& arg_types);
 
     [[noreturn]] void error_lex(int lineno);
 
     [[noreturn]] void error_syn(int lineno);
 
-    [[noreturn]] void error_undef(int lineno, const string& id);
+    [[noreturn]] void error_undef(int lineno, const std::string& id);
 
-    [[noreturn]] void error_def(int lineno, const string& id);
+    [[noreturn]] void error_def(int lineno, const std::string& id);
 
-    [[noreturn]] void error_undef_func(int lineno, const string& id);
+    [[noreturn]] void error_undef_func(int lineno, const std::string& id);
 
     [[noreturn]] void error_mismatch(int lineno);
 
-    [[noreturn]] void error_prototype_mismatch(int lineno, const string& id, vector<string>& arg_types);
+    [[noreturn]] void error_prototype_mismatch(int lineno, const std::string& id, std::vector<std::string>& arg_types);
 
     [[noreturn]] void error_unexpected_break(int lineno);
 
@@ -36,7 +35,7 @@ namespace output
 
     [[noreturn]] void error_main_missing();
 
-    [[noreturn]] void error_byte_too_large(int lineno, const string& value);
+    [[noreturn]] void error_byte_too_large(int lineno, const std::string& value);
 }
 
 #endif
